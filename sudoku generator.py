2e99b45d5f81f467baa6b2e9731ece85a400dc65
Oliwier Kossak <oliwierkossak109@gmail.com>
2025-09-02 10:08:46 +0200

    add method to evalute board

--- sudoku generator.py
+++ sudoku generator.py
@@ -19,14 +19,38 @@ class Sudoku():
             print(f"{index} : {row}")
 
     def generate_start_population(self):
+        '''Generate start population with sudoku boards'''
         for i in range(self.population_number):
             sudoku_board = self._init_sudoku_random_board()
             self.population.append(sudoku_board)
 
+    def evaluate_sudoku_board(self, board):
+        board_score = 0
+        print(self._evalute_row(board))
+        print(self._evalute_column(board))
 
+    def _evalute_row(self, board):
+        rows_score = 0
+        for row in board:
+            start_row_score = 45
+            current_row_score = sum(row)
+            start_row_score -= current_row_score
+            rows_score += abs(start_row_score)
+        return rows_score
+
+    def _evalute_column(self, board):
+        columns_score = 0
+        transpose_list = list(map(list,zip(*board)))
+        for row in transpose_list:
+            start_row_score = 45
+            current_row_score = sum(row)
+            start_row_score -= current_row_score
+            columns_score += abs(start_row_score)
+        return columns_score
 
 sudoku = Sudoku(2)
 sudoku.generate_start_population()
+sudoku.evaluate_sudoku_board(sudoku.population[1])
 for i in range(2):
     sudoku.print_board(sudoku.population[i])
     print("-"*30)
